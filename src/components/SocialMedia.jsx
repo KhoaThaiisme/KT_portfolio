3cbb46223ea92c92f6a85c04b8e39e3cfaef941f
KhoaThaiisme <khoathai@khoas-MBP.attlocal.net>
2022-10-06 15:18:01 -0500

    first commit

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
+
+const SocialMedia = () => {
+  return (
+    <div className='app__social'> 
+        <div>
+            <a href='https://twitter.com/KhoaThai2' target='_blank'><BsTwitter/></a>
+        </div>
+        <div>
+            <a href="https://www.instagram.com/iahtaohk/" target='_blank'><BsInstagram /></a>
+        </div>
+        <div>
+            <a href="https://www.facebook.com/khoa.thai1/" target='_blank'><FaFacebookF /></a>
+        </div>
+    </div>
+  )
+}
+
+export default SocialMedia
