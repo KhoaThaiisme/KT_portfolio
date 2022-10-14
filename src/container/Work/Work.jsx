bb60ef0b1d4de846551f5d9e221bb3b73b20b0d1
KhoaThaiisme <khoathai@khoas-MBP.attlocal.net>
2022-10-14 13:56:05 -0500

    few changes

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -48,7 +48,7 @@ const Work = () => {
     </h2>  
   
     <div className='app__work-filter'>
-      {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+      {['UI/UX', 'Sass', 'Tailwinds', 'React JS', 'All'].map((item, index) => (
         <div 
           key={index}
           onClick={() => handleWorkFilter(item )}
